79fef2054436af5055d5fa0ec47dda56b8ab94db
Leandro Lago da Silva <leandro@datacampo.com.br>
2022-06-07 19:49:58 -0300

    header responsive

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,16 +1,23 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react"
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({showProfileData = true}: ProfileProps) {
   return (
+    
     <Flex align="center">
-      <Box mr="4">
-        <Text textAlign="right">
-          Leandro L. Silva
-        </Text>
-        <Text color="gray.300" textAlign="right" fontSize="small">
-          leandro@datacampo.com.br
-        </Text>
-      </Box>
+      { showProfileData && (
+        <Box mr="4">
+          <Text textAlign="right">
+            Leandro L. Silva
+          </Text>
+          <Text color="gray.300" textAlign="right" fontSize="small">
+            leandro@datacampo.com.br
+          </Text>
+        </Box>
+      )}
       <Avatar size="md" name="Leandro L. Silva" src="https://github.com/lemaun.png" />
     </Flex>
 
