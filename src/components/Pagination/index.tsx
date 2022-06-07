7969361c60ed723868eb0a65c810202ceb321386
Leandro Lago da Silva <leandro@datacampo.com.br>
2022-06-07 18:05:35 -0300

    pagination component test

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -13,10 +13,13 @@ export function Pagination() {
         <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
       </Box>
       <HStack spacing="2">
-        <PaginationItem isCurrent={true} number={1} />
+        <PaginationItem isCurrent number={1} />
         <PaginationItem number={2} />
         <PaginationItem number={3} />
         <PaginationItem number={4} />
+        <PaginationItem number={5} />
+        <PaginationItem number={6} />
+        <PaginationItem number={7} />
       </HStack>
     </HStack>
   )
