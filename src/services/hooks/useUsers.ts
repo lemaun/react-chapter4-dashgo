b1097254b0982e598af70726d20fa47bb1561a33
Leandro Lago da Silva <leandro@datacampo.com.br>
2022-06-21 20:20:23 -0300

    adjust var name

--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -27,7 +27,7 @@ export async function getUsers(page: number): Promise<GetUsersResponse> {
       id: user.id,
       name: user.name,
       email: user.email,
-      createdAt: new Date(user.createdAt).toLocaleDateString('pt-Br', {
+      createdAt: new Date(user.created_at).toLocaleDateString('pt-Br', {
         day: '2-digit',
         month: 'long',
         year: 'numeric'
